26a893a5756ae979e72778f19493152d81fbdebc
adrianag4 <adriana.gutierrez235633@potros.itson.edu.mx>
2024-09-10 00:13:18 -0700

    capa presentacion

---
+++ ListasCapaPresentacion/src/main/java/com/mycompany/listascapapresentacion/ListasCapaPresentacion.java
@@ -0,0 +1,15 @@
+package com.mycompany.listascapapresentacion;
+
+import interfaz.frmPrincipal;
+
+/**
+ *
+ * @author adria
+ */
+public class ListasCapaPresentacion {
+
+    public static void main(String[] args) {
+        frmPrincipal principal = new frmPrincipal();
+        principal.setVisible(true);
+    }
+}
